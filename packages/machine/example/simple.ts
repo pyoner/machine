16947d439acad7585a3e0fe1b4338ff5fb10bf7e
Jungle <devex.soft@gmail.com>
2020-04-25 19:45:57 +0500

    chore(example): Change context in the example

--- packages/machine/example/simple.ts
+++ packages/machine/example/simple.ts
@@ -19,9 +19,13 @@ interface PressEvent extends Event<"press"> {
   charCode: number;
 }
 
+interface MyContext extends Context {
+  flag: boolean;
+}
+
 type Events = ClickEvent | PressEvent;
 
-type S1State = State<"s1", Context, Events>;
+type S1State = State<"s1", MyContext, Events>;
 
 export const s1: S1State = {
   id: "s1",
@@ -36,17 +40,13 @@ export const s1: S1State = {
     }
   }
 };
-export const s11 = state<"s11", Context, Events>("s11", {
-  click: transition("StateClick", guards((_, e) => e.y > 100 && e.x > 200)),
-  press: transition("StatePress", guards((_, e) => e.charCode > 100))
-});
 
 interface EnterEvent extends Event<"enter"> {
   value: string;
 }
 
 type S2Events = EnterEvent | ClickEvent;
-type S2State = State<"s2", Context, S2Events>;
+type S2State = State<"s2", MyContext, S2Events>;
 export const s2: S2State = {
   id: "s2",
   transitions: {
@@ -65,3 +65,9 @@ export const states: States<S1State | S2State> = {
   s1,
   s2
 };
+
+// use helpers
+export const s11 = state<"s11", MyContext, Events>("s11", {
+  click: transition("StateClick", guards((_, e) => e.y > 100 && e.x > 200)),
+  press: transition("StatePress", guards((_, e) => e.charCode > 100))
+});
